7e8c53a8df37f3abd52dea2c5ca9fcedc74c1cad
Igorekmog <m19_bond@tut.by>
2020-03-07 17:03:25 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!Array.isArray(array) || (array.length <= 0)){
+        return 0;
+    }
+minValue=array[0];
+for(let i=1;i<array.length;i++)
+        if(array[i]<minValue)
+        {minValue=array[i];}
+    return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if(!Array.isArray(array) || (array.length <= 0)){
+        return 0;
+    }
+    maxValue=array[0];
+    for(let i=1;i<array.length;i++)
+        if(array[i]>maxValue)
+        {maxValue=array[i];}
+    return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!Array.isArray(array) || (array.length <= 0)){
+        return 0;
+    }
+    let avgValue = array.reduce((avgValue, current) => avgValue + current, 0);
+  return avgValue/array.length;
 }
